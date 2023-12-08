e7e1a7015495cc0d708a102ea394aa5c8b0a6153
Neo40 <tron36.cc@gmail.com>
2023-12-07 23:16:35 -0300

    Criação de classe com atributos

---
+++ src/FuncionarioPJ.java
@@ -0,0 +1,8 @@
+public abstract class FuncionarioPJ {
+    private String nome;
+    private String documento;
+    private Endereco endereco;
+    private Double horasTrabalhadas;
+    private Double valorHora;
+    private Double valorRemuneracao;
+}
