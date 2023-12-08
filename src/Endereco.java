f05c51f95e691cd49d4f52368a4dc98d70bcdce4
Neo40 <tron36.cc@gmail.com>
2023-12-07 21:28:27 -0300

    Adição de construtores

--- src/Endereco.java
+++ src/Endereco.java
@@ -3,4 +3,12 @@ public class Endereco {
     private String complemento;
     private String bairro;
 
+    public Endereco(String rua, String complemento, String bairro) {
+        this.rua = rua;
+        this.complemento = complemento;
+        this.bairro = bairro;
+    }
+
+    public Endereco() {
+    }
 }
