3e0892659a49d828418f8e87866df2a2a5096b68
Neo40 <tron36.cc@gmail.com>
2023-12-07 22:09:05 -0300

    criação de método toString

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,4 +1,14 @@
 public class Vendedor extends FuncionarioCLT {
     private Double valorBonificacao;
-    
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "valorBonificacao=" + valorBonificacao +
+                ", nome='" + nome + '\'' +
+                ", documento='" + documento + '\'' +
+                ", valorSalario=" + valorSalario +
+                ", endereco=" + endereco +
+                '}';
+    }
 }
