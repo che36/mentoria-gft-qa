50bee01960bf2de1957a95729049a0c3f7a50194
Neo40 <tron36.cc@gmail.com>
2023-12-07 23:31:33 -0300

    criação de método toString

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,3 +1,13 @@
 public class Gerente extends FuncionarioPJ {
-
+    @Override
+    public String toString() {
+        return "Gerente{" +
+                "nome='" + super.getNome() + '\'' +
+                ", documento='" + super.getDocumento() + '\'' +
+                ", endereco=" + super.getEndereco() +
+                ", horasTrabalhadas=" + super.getHorasTrabalhadas() +
+                ", valorHora=" + super.getValorHora() +
+                ", valorRemuneracao=" + super.getValorRemuneracao() +
+                '}';
+    }
 }
