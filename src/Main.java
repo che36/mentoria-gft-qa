72b7e7e9ad1a7f3eff2ff3009e5d919238cdc0f1
Neo40 <tron36.cc@gmail.com>
2023-12-07 22:08:05 -0300

    criação de 2 objetos

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Endereco endereco = new Endereco("Rua do Endereco", "Complemento do Endereco", "Bairro do Endereco");
+        System.out.println(endereco);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Vendedor vendedor = new Vendedor();
+        vendedor.setNome("Cesar Constanzo");
+        vendedor.setDocumento("13646546");
+        vendedor.setValorSalario(1000d);
+        vendedor.setEndereco(endereco);
+        System.out.println(vendedor);
     }
 }
