b70cc6e9a055ce47bd1e316602cbc1e14ecf52f6
Neo40 <tron36.cc@gmail.com>
2023-12-07 23:20:24 -0300

    Criação de getters and setters

--- src/FuncionarioPJ.java
+++ src/FuncionarioPJ.java
@@ -9,4 +9,52 @@ public abstract class FuncionarioPJ {
     public void calculaRemuneracao() {
         this.valorRemuneracao = this.horasTrabalhadas * this.valorHora;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Double getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(Double horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public Double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(Double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public Double getValorRemuneracao() {
+        return valorRemuneracao;
+    }
+
+    public void setValorRemuneracao(Double valorRemuneracao) {
+        this.valorRemuneracao = valorRemuneracao;
+    }
 }
